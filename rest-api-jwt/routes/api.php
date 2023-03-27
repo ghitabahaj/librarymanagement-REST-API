c90e5ba31d1966994923a75aa98b5570fd98f9c2
Ghita <bahajghita3@gmail.com>
2023-03-27 11:26:10 +0000

    my project

--- rest-api-jwt/routes/api.php
+++ rest-api-jwt/routes/api.php
@@ -22,6 +22,7 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+    Route::post('me', 'me');
 });
 
 Route::resource('Livre',LivreController::class);
