734ed416891f5a8c83a0d9d074262049deeff83a
Ghita <bahajghita3@gmail.com>
2023-03-27 11:21:23 +0000

    my project

--- rest-api-jwt/app/Http/Controllers/GenreController.php
+++ rest-api-jwt/app/Http/Controllers/GenreController.php
@@ -8,6 +8,12 @@ use App\Http\Requests\UpdateGenreRequest;
 
 class GenreController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      *
